8af5b1ffca0449f4635bc1fe002aa16a09d756a5
Sami Sabir-Idrissi <sami_sabir-idrissi@epam.com>
2023-06-14 19:13:24 -0400

    processing large files

---
+++ src/test/java/FileReaders/LookForLastIndex.java
@@ -0,0 +1,47 @@
+package FileReaders;
+
+import org.junit.Assert;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class LookForLastIndex {
+
+    public static void main(String[] args) {
+        String filePath = "src/test/java/FileReaders/datafile.txt";
+
+        Map<Character, Integer> charCounts = new HashMap<>();
+        charCounts.put(' ', 0); // Null character count
+        charCounts.put('F', 0); // 'F' character count
+        charCounts.put('M', 0); // 'M' character count
+        charCounts.put('A', 0); // 'A' character count
+        charCounts.put('R', 0); // 'R' character count
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+
+            while((line = reader.readLine()) != null){
+                if (line != null && line.length() >= 85) {
+                    char character = line.charAt(85);
+                    System.out.println("Character: " + character);
+                    // Increment the count for the character
+                    charCounts.put(character, charCounts.get(character) + 1);
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Print the character counts
+        for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
+            System.out.println("Character: " + entry.getKey() + ", Count: " + entry.getValue());
+        }
+    }
+}
--- src/test/java/SwichCase/EnhancedSwitchCasePrac.java
+++ src/test/java/SwichCase/EnhancedSwitchCasePrac.java
@@ -6,17 +6,17 @@ public class EnhancedSwitchCasePrac {
     public static void main(String[] args) {
         int stage = 3;
 
-        String season = switch(stage){
-            case 0 -> "spring";
-            case 1 -> "summer";
-            case 2 -> "fall";
-            case 3 -> "winter";
-            default -> {
-                System.out.println("season value is not what is expected");
-                yield "invalid stage";
-            }
-        };
+//        String season = switch(stage){
+//            case 0 -> "spring";
+//            case 1 -> "summer";
+//            case 2 -> "fall";
+//            case 3 -> "winter";
+//            default -> {
+//                System.out.println("season value is not what is expected");
+////                yield "invalid stage";
+//            }
+//        };
 
-        System.out.println(season);
+//        System.out.println(season);
     }
 }
